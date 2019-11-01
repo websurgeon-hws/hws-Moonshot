feb742e5bce8730eb9f156efb646ec67895d3338
Peter Barclay <peter@peter-barclay.com>
2019-11-01 08:16:37 +0000

    Working with hierarchical Codable data

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -4,15 +4,34 @@
 
 import SwiftUI
 
+struct User: Codable {
+    var name: String
+    var address: Address
+}
+
+struct Address: Codable {
+    var street: String
+    var city: String
+}
+
 struct ContentView: View {
     var body: some View {
-        NavigationView {
-            List(0..<100) { row in
-                NavigationLink(destination: Text("Detail View \(row)")) {
-                    Text("Row \(row)")
+        Button("Decode JSON") {
+            let input = """
+            {
+                "name": "Taylor Swift",
+                "address": {
+                    "street": "555, Taylor Swift Avenue",
+                    "city": "Nashville"
                 }
             }
-            .navigationBarTitle("SwiftUI")
+            """
+
+            let data = Data(input.utf8)
+            let decoder = JSONDecoder()
+            if let user = try? decoder.decode(User.self, from: data) {
+                print(user.address.street)
+            }
         }
     }
 }
