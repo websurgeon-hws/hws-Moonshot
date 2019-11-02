262f03e41c1d15614a3b1b9a635fc38bb72fc15d
Peter Barclay <peter@peter-barclay.com>
2019-11-02 07:41:38 +0000

    Formatting our mission view

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,9 +9,23 @@ struct ContentView: View {
     let missions: [Mission] = Bundle.main.decode("missions.json")
 
     var body: some View {
-        Group {
-            Text("Astronauts: \(astronauts.count)")
-            Text("Missions: \(missions.count)")
+        NavigationView {
+            List(missions) { mission in
+                NavigationLink(destination:
+                    Text("Detail View")
+                ) {
+                    Image(mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 44, height: 44)
+                    VStack(alignment: .leading) {
+                        Text(mission.displayName)
+                            .font(.headline)
+                        Text(mission.formattedLaunchDate)
+                    }
+                }
+            }
+            .navigationBarTitle("Moonshot")
         }
     }
 }
